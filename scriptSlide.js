d86763cf94a9ad39dc1fd9ff93f4b6546a8940a7
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-09 15:01:47 -0300

    Create scriptSlide.js

---
+++ scriptSlide.js
@@ -0,0 +1,16 @@
+/*<![CDATA[*/
+//Profile photo resize script
+$(document).ready(function(){$("#main-slider div img",$(this)).each(function(){var src=$(this).attr("src").replace(/\/s[0-9]+/g,'/h500-w600-c');$(this).attr("src",src)})});
+/*]]>*/
+</script>
+<script type='text/javascript'>
+/*<![CDATA[*/
+//Profile photo resize script
+$(document).ready(function(){$("#Profile1 a img",$(this)).each(function(){var src=$(this).attr("src").replace(/\/s[0-9]+/g,'/s250-c');$(this).attr("src",src)})});
+/*]]>*/
+</script>
+<script type='text/javascript'>
+/*<![CDATA[*/
+//Profile photo resize script
+$(document).ready(function(){$("#Image99 img",$(this)).each(function(){var src=$(this).attr("src").replace(/\/s[0-9]+/g,'/s250-c');$(this).attr("src",src)})});
+/*]]>*/
