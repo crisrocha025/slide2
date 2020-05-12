999bd2ca2da452e097f522f763da0308f5a39e26
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-12 10:06:58 -0300

    Update PopularPost.js

--- PopularPost.js
+++ PopularPost.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){$(&quot;.item-thumbnail&quot;).find(&quot;img&quot;).attr(&#39;src&#39;, function(i, src) {return src.replace( &#39;w72-h72-p-nu&#39;, &#39;s500-c&#39; );})});
 $(document).ready(function(){$(&quot;.item-thumbnail&quot;).find(&quot;img&quot;).attr(&#39;src&#39;, function(i, src) {return src.replace( &#39;w72-h72-p-k-no-nu&#39;, &#39;s500-c&#39; );})});
-
- 
-
 $(document).ready(function(){$(&quot;.mobile-index-thumbnail&quot;).find(&quot;img&quot;).attr(&#39;src&#39;, function(i, src) {return src.replace( &#39;s72-c&#39;, &#39;s1600&#39; );})});
