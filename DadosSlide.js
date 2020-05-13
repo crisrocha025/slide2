797a69070b77129f7a98cfa2cf2894496354a7a2
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-13 11:07:53 -0300

    Update DadosSlide.js

--- DadosSlide.js
+++ DadosSlide.js
@@ -13,24 +13,24 @@ itensDom.push(doc)
 var ant = document.querySelector('.ant')
 var prox = document.querySelector('.prox')
 function anterior(){
-if(itensDom[0].style=='z-index:9;'){	
+if(itensDom[0].style=='z-index:5;'){	
   itensDom[4].style='z-index:9;'
   itensDom[0].style='z-index:1;'
 }
 else if(itensDom[4].style=='z-index:9;'){	
-  itensDom[3].style='z-index:9;'
+  itensDom[3].style='z-index:8;'
   itensDom[4].style='z-index:1;'
 }
 else if(itensDom[3].style=='z-index:9;'){	
-  itensDom[2].style='z-index:9;'
+  itensDom[2].style='z-index:7;'
   itensDom[3].style='z-index:1;'
 }
 else if(itensDom[2].style=='z-index:9;'){	
-  itensDom[1].style='z-index:9;'
+  itensDom[1].style='z-index:6;'
   itensDom[2].style='z-index:1;'
 }
 else{	
-  itensDom[0].style='z-index:9;'
+  itensDom[0].style='z-index:5;'
   itensDom[1].style='z-index:1;'
 }
 }
