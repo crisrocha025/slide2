466960e23cbfddccc45d54315f6c06ba28d9fc2e
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-13 09:40:18 -0300

    Create DadosSlide.js

---
+++ DadosSlide.js
@@ -0,0 +1,17 @@
+var itens=[]
+	var num
+    for(num=1;num < 6;num++){
+      var recebedor = '.itensPosts:nth-child('+num+')'
+	  itens.push(recebedor)
+    }
+var itensDom = []
+var cont
+for(cont=0;cont<5;cont++){
+var doc = document.querySelector(itens[cont])
+itensDom.push(doc)
+}
+var ant = document.querySelector('.ant')
+var prox = document.querySelector('.prox')
+function anterior(){
+  itensDom[4].style='border:2px solid red;'
+}
