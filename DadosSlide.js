f7c7206b1aea01bd8bdbc24920f19e76cb1b46b6
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-13 14:08:18 -0300

    Update DadosSlide.js

--- DadosSlide.js
+++ DadosSlide.js
@@ -10,4 +10,20 @@ for(cont=0;cont<5;cont++){
 var doc = document.querySelector(itens[cont])
 itensDom.push(doc)
 }
-
+window.load = slide(0)
+var numItem = 0;
+  function slide(n){
+  itensDom[n].style = 'z-index:9;'
+  }
+  function anterior(){
+   if(numItem<5){
+	slide(numItem)
+	numItem++
+   }
+  }
+function proximo(){
+  if(numItem>0){
+	slide(numItem)
+	numItem--
+  }
+  }
